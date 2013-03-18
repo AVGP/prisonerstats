64be2f5d129807f85217e03c1ffa3d1d88a10ce2
root <root@zero.geekonaut.de>
2013-03-18 14:46:34 +0000

    Initial setup of Scrapy

---
+++ prisonstat/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for prisonstat project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'prisonstat'
+
+SPIDER_MODULES = ['prisonstat.spiders']
+NEWSPIDER_MODULE = 'prisonstat.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'prisonstat (+http://www.yourdomain.com)'
